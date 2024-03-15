3652ccf88c34427cd9fc956e1e72bf2e9f1aa23a
Caramelo234 <163418531+Caramelo234@users.noreply.github.com>
2024-03-15 10:07:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ2LCV1qF8NI00CuRDXMLRjdgwLsVZRuYH16YY_btJ7g2vNHjgz"
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://flowgames.gg/wp-content/uploads/2023/12/turma-da-monica-one-piece-gibi-1044x774.jpg"
+}
